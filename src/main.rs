de1ea4fd10c7e6134db43c874b4ebe3b48ba37fd
Evan <evanne.scherrer@gmail.com>
2024-04-22 16:25:29 -0500

    word prompt + comment game loop

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,60 @@
 mod game;
 
-use std::{io::{Read, Write}, net::{TcpListener, TcpStream}};
+use std::{io::{self, Read, Write}, net::{TcpListener, TcpStream}};
 use game::GameState;
 
 const IP_ADDRESS: &str = "127.0.0.1:25565";
 
 fn main() {
-    // TODO: prompt for word on startup - limit to 14 ASCII chars
-    let word = "ballet"; // limit of 14 ASCII characters - enforce that
-    let mut game: GameState = initialize_game(word);
+    // prompt user for a word
+    let word = prompt_for_word();
 
+    // initialize the game with the user's word
+    let mut game: GameState = initialize_game(&word as &str);
+    println!("Game initialized with word {}", &word);
+
+    // start listening for data from the clients
     let listener = TcpListener::bind(IP_ADDRESS).unwrap();
 
-    // individual "packet"
     for stream in listener.incoming() {
         let stream = stream.unwrap();
 
-        println!("Connection established! {:?}", stream);
+        // handle individual packets & modify the GameState
         handle_connection(stream, &mut game);
     }
+}
+
+// prompt the user for a word to play the game with
+fn prompt_for_word() -> String {
+    println!("Please input a word, 2-14 ASCII characters");
+    let mut word = String::new();
+
+    loop {
+        // empty out word buffer on each iteration of the loop to clear out previous input attempt
+        word.clear();
+        
+        io::stdin()
+        .read_line(&mut word)
+        .expect("Failed to read line");
+        
+        // trim whitespace and convert to uppercase
+        word = word.trim().to_string().to_ascii_uppercase();
+
+        if word.len() < 2 || word.len() > 14 {
+            println!("Word must be between 2 and 14 characters long");
+            continue; // restart loop to prompt for input again
+        }
+
+        if !word.chars().all(|c| c.is_ascii()) {
+            println!("Word must contain only ASCII characters");
+            continue; // restart loop to prompt for input again
+        }
+
+        // all checks passed, exit loop
+        break; 
+    }
+
+    word
 }
 
 // initialize game with given word
@@ -27,6 +63,7 @@ fn initialize_game(word: &str) -> GameState {
     game
 }
 
+// process incoming packets
 fn handle_connection(mut stream: TcpStream, game: &mut GameState) {
     // create an empty buffer to read data into
     let mut guess_buffer = [0; 16];
