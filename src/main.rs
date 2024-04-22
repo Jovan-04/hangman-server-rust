73421a3248e3f005901035bbce03c397d02edc01
Evan <evanne.scherrer@gmail.com>
2024-04-22 11:04:12 -0500

    add comments

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,14 @@ mod game;
 use std::{io::{Read, Write}, net::{TcpListener, TcpStream}};
 use game::GameState;
 
+const IP_ADDRESS: &str = "127.0.0.1:25565";
 
 fn main() {
-    let mut game: GameState = initialize_game();
+    // TODO: prompt for word on startup - limit to 14 ASCII chars
+    let word = "ballet"; // limit of 14 ASCII characters - enforce that
+    let mut game: GameState = initialize_game(word);
 
-    let listener = TcpListener::bind("127.0.0.1:25565").unwrap();
+    let listener = TcpListener::bind(IP_ADDRESS).unwrap();
 
     // individual "packet"
     for stream in listener.incoming() {
@@ -18,25 +21,25 @@ fn main() {
     }
 }
 
-fn initialize_game() -> GameState {
-    let word = "ballet"; // limit of 14 ASCII characters
+// initialize game with given word
+fn initialize_game(word: &str) -> GameState {
     let game = GameState::new(&word);
     game
 }
 
-
 fn handle_connection(mut stream: TcpStream, game: &mut GameState) {
+    // create an empty buffer to read data into
     let mut guess_buffer = [0; 16];
 
+    // read 16 bytes of data into the buffer
     let _ = stream.read(&mut guess_buffer);
     println!("read data from client {:?}", guess_buffer);
-    // update game state
+
+    // TODO: update game state based on buffer (see protocol-doc)
 
     // reply new GS to client
     let to_write = &game.serialize();
     // println!("{:?}", &to_write);
     let _ = stream.write(to_write);
-
-    // let _ = stream.write(&[b'h', b'e', b'l', b'l', b'o', b'w', b'o', b'r', b'l', b'd']);
 
 }
